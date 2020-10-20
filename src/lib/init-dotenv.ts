775f47206e9d0a5308a63eac8d125f93488d68ae
Viacheslav Voytovich <voytovich.slava@gmail.com>
2020-10-20 23:42:00 +0700

    dotenv intializer

---
+++ src/lib/init-dotenv.ts
@@ -0,0 +1,17 @@
+import fs from 'fs';
+import path from 'path';
+import dotenv from 'dotenv';
+
+const projectDir = path.resolve(__dirname, '../..');
+const nodeEnv = process.env.NODE_ENV;
+
+if (nodeEnv && nodeEnv !== 'production') {
+  const fullEnvPath = path.resolve(projectDir, `.env.${nodeEnv}`);
+  const shortEnvPath = path.resolve(projectDir, '.env');
+
+  if (fs.existsSync(fullEnvPath)) {
+    dotenv.config({ path: fullEnvPath });
+  } else {
+    dotenv.config({ path: shortEnvPath });
+  }
+}
