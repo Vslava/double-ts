7834d61ff3fd502f3382cd368356112b43a52779
Viacheslav Voytovich <voytovich.slava@gmail.com>
2020-11-30 22:45:32 +0700

    make more readable code

--- src/services/util/is-file-exist.ts
+++ src/services/util/is-file-exist.ts
@@ -1,6 +1,11 @@
 import fs from 'fs';
 
 export default async (filePath: string): Promise<boolean> => {
-  const r = await fs.promises.access(filePath).catch((err) => err);
-  return typeof r === 'undefined';
+  try {
+    await fs.promises.access(filePath);
+
+    return true;
+  } catch (err) {
+    return false;
+  }
 };
